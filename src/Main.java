176a13aff21cccf9782a137920bc71d6815ca494
YevheniiYevtushenko <114355291+YevheniiYevtushenko@users.noreply.github.com>
2022-09-26 02:12:22 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("This task done by Yevtushenko Yevheniy ПЗ-19-1/9");
         System.out.println("25.09.2022");
+        // Коментар, доданий з браузера
     }
 }
