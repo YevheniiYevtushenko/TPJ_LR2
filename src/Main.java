2f80e93d4ac091146a5ed82d0e1196094f62d80f
YevheniiYevtushenko <yevheniiyevtushenkov@gmail.com>
2022-09-26 01:05:20 +0300

    New Commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world");
+        System.out.println("This task2 done by Yevtushenko Yevheniy ПЗ-19-1/9");
+    }
+}
